8a7b0e7db41a75ba4fb30d6b61fc35480330c25b
shravani.vallurupalli <svallurupalli@jetblue.com>
2021-04-25 20:12:55 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 
 const app = express();
+const port = process.env.PORT || 3000
 
 const publicPathDirectory = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -86,7 +87,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-console.log('Listening to port 3000!!')
+app.listen(port, () => {
+console.log(`Server is up at ${port}!!!`)
 })
 
